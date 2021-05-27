9e2962c896b93916343e8e90c1c73a708cd43195
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-05-27 10:38:36 +0200

    Switch to Maven Central

--- src/main/kotlin/io/github/robinnunkesser/explicitarchitecture/tunesearch/core/ports/SearchTracksCommand.kt
+++ src/main/kotlin/io/github/robinnunkesser/explicitarchitecture/tunesearch/core/ports/SearchTracksCommand.kt
@@ -1,6 +1,6 @@
 package io.github.robinnunkesser.explicitarchitecture.tunesearch.core.ports
 
-import de.hshl.isd.explicitarchitecture.Command
+import io.github.robinnunkesser.explicitarchitecture.Command
 
 interface SearchTracksCommand : Command<SearchTracksDTO, List<CollectionEntity>> {
 }
--- src/main/kotlin/io/github/robinnunkesser/explicitarchitecture/tunesearch/core/ports/TunesSearchEngine.kt
+++ src/main/kotlin/io/github/robinnunkesser/explicitarchitecture/tunesearch/core/ports/TunesSearchEngine.kt
@@ -1,5 +1,5 @@
 package io.github.robinnunkesser.explicitarchitecture.tunesearch.core.ports
 
 interface TunesSearchEngine {
-    suspend fun getSongs(term: String): Result<List<TrackEntity>>
+    suspend fun getSongs(term: String): List<TrackEntity>
 }
